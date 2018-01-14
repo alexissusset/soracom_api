64cdf81877376dabcb409b5881fac9a4fd1fcb87
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2018-01-14 15:25:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# About soracom_api
+
+## install
+npm install soracom_api
+
+## how to use
+"authKey" and "token" are checked automatically in this API.
+
+### authorization
+The API supports the both of authorization methods: email/password and authKey and secret.
+
+```
+var soracom = new Soracom({email: 'mail address',password:'password'});
+```
+```
+var soracom = new Soracom({authKeyId: 'keyId',authKey:'secret'});
+```
+
+When this API accesses SORACOM service, it takes authKey and token automatically.
+
+### get information of SIM
+
+```
+var Soracom = require('soracom_api');
+var soracom = new Soracom({email: 'mail address',password:'password'});
+soracom.get('/subscriber/imsi',function(err,res){
+});
+```
+
+### set name into SIM
+
+```
+var Soracom = require('soracom_api');
+var soracom = new Soracom({authKeyId: 'keyId',authKey:'secret'});
+soracom.post('/subscriber/imsi/tags',[
+  {
+    "tagName": "name",
+    "tagValue": "my_test_sim"
+  }
+],function(err,res){
+  console.log({err:err,res:res});
+});
+
+```
+
+## API
+### soracom.get('path',function(err,res){ callback });
+parameters:
+ - path:  request path
+return:
+ - err: error message
+ - res: response
+
+### soracom.post('path',params,function(err,res){ callback });
+- parameters:
+ - path:  request path
+ - params: request body. it can be omitted.
+- return:
+ - err: error message
+ - res: response
+
+### soracom.put('path',params,function(err,res){ callback });
+- parameters:
+ - path:  request path
+ - params: request body. it can be omitted.
+
+- return:
+ - err: error message
+ - res: response
+
+### soracom.delete('path',params,function(err,res){ callback });
+- parameters:
+ - path:  request path
+ - params: request body. it can be omitted.
+- return:
+ - err: error message
+ - res: response
---
+++ soracom_api.js
@@ -0,0 +1,122 @@
+'use strict'
+
+/* Soracom API
+*	var Soracom = require('./soracom');
+*	var soracom = new Soracom({email: 'mail address',password:'password'});
+*	var soracom = new Soracom({authKeyId: 'keyId',authKey:'secret'});
+*/
+module.exports = function(obj) {
+	const https = require('https');
+	const HOST = "api.soracom.io";
+	const PATH = '/v1';
+	var node = this;
+	node.login = obj;
+	node.session = {
+		headers: {
+			"Accept": "application/json",
+			"Content-Type": "application/json",
+		}
+	};
+	if (node.login.tokenTimeoutSeconds === undefined) {
+		node.login.tokenTimeoutSeconds = 86400;
+	}
+	node.get = function(path,callback) {
+		httpRequest('GET',path,function(err,res){
+			callback(err,res);
+		});
+	};
+	node.post = function(path, params,callback) {
+		httpRequest('POST',path,params,function(err,res){
+			callback(err,res);
+		});
+	};
+	node.put = function(path, params,callback) {
+		httpRequest('PUT',path,params,function(err,res){
+			callback(err,res);
+		});
+	}
+	node.delete = function(path, params,callback) {
+		httpRequest('DELETE',path,params,function(err,res){
+			callback(err,res);
+		});
+	}
+	function getToken(callback) {
+		var options = {
+			host: HOST,
+			path: PATH+'/auth',
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		}
+		var req = https.request(options, (res) => {
+			res.setEncoding('utf8');
+			res.on('data', (chunk) => {
+				var res = JSON.parse(chunk);
+				try {
+					node.session.headers["X-Soracom-API-Key"] = res.apiKey;
+					node.session.operatorId = res.operatorId;
+					node.session.userName = res.userName;
+					node.session.headers["X-Soracom-Token"] = res.token;
+					node.session.timeout = (new Date()).getTime() + node.login.tokenTimeoutSeconds;
+					callback(null,node.session);
+				} catch(e) {
+					callback('login error.',null);
+				}
+			});
+			req.on('error', (e) => {
+				callback(e.message,null);
+			});
+		});
+		req.write(JSON.stringify(node.login));
+		req.end();
+	}
+	function checkToken(callback) {
+		if (typeof node.session === "undefined" ) {
+			if (node.session.timeout !== undefined) {
+				if (node.session.timeout < (new Date()).getTime()){
+					callback(null,true);
+					return;
+				}
+			}
+		}
+		getToken(function(err,res){
+			callback(err,res);
+		});
+	}
+	function httpRequest(method,path,params,callback) {
+		if(typeof params === 'function') { callback = params; }
+		checkToken(function(err,res){
+			if(err === null) {
+				var options = {
+					host: HOST,
+					path: PATH+path,
+					method: method,
+					headers: node.session.headers
+				};
+				var req = https.request(options, (res) => {
+					res.setEncoding('utf8');
+					res.on('data', (chunk) => {
+						var res = JSON.parse(chunk);
+						callback(null,res);
+					});
+					req.on('error', (e) => {
+						callback(e,null);
+					});
+				});
+				if(method !== 'GET') {
+					if(typeof params !== 'function') {
+						req.write(JSON.stringify(params));
+					} else {
+						req.write(JSON.stringify({}));
+					}
+				}
+				req.end();
+			} else {
+				callback(err,null);
+			}
+		});
+	}
+};
+
